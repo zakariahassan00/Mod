b583fa810b16ab9f89f6aa7b9fdd4ebbf2803147
Zakaria Hassan <zakariahassan00@hotmail.com>
2019-10-10 07:12:32 +0200

    added redux

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,59 +1,45 @@
-import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { connect } from "react-redux"
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import MuiTextFields from '../common/input/MuiTextField'
-import "./login.css";
+import { loginStyles } from './loginStyles'
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-  backgroundColor: "#1e242c  ",
-  borderRadius: 15,
-  },
-  typography: {
-    color: "#fff",
-    marginBottom: 10
-  },
-  button: {
-    margin: 20,
-    width: 300,
-    color: "#fff",
-    textTransform: 'capitalize'
-  },
-  googleButton: {
-    width: 300,
-    color: "#fff",
-    backgroundColor: "#d34836",
-    textTransform: 'capitalize',
-    "&:hover": {
-      backgroundColor: "#a83c3c"
-    },
+
+class Login extends Component {
+
+  render() {
+    console.log(this.props.loginReducer)
+    const { classes } = this.props
+    return (
+      <div className={classes.modLogin}>
+        <div className={classes.modLoginBg} />
+        <Paper className={classes.paper}>
+          <Typography className={classes.typography} variant="h4" gutterBottom>
+            Log into Mod
+          </Typography>
+          <MuiTextFields id="mod-login-email" label="Email" type="email" name="email"/>
+          <MuiTextFields id="mod-login-password" label="Password" type="password" name="password"/>
+          <div>
+            <Button size="large" variant="contained" color="primary" className={classes.button}>
+              Sign in
+            </Button>
+          </div>
+          <div>
+            <Button size="large" variant="contained" className={classes.googleButton}>
+              Login With Google
+            </Button>
+          </div>
+        </Paper>
+      </div>
+    );
   }
-}));
-
-export default function Login() {
-  const classes = useStyles();
-  return (
-    <div className="mod-login ">
-      <div className="mod-login-bg" />
-      <Paper classes={ {root: classes.paper}} className="mod-login-form">
-      <Typography classes={ {root: classes.typography}}  variant="h4" gutterBottom>
-        Log into Mod
-      </Typography>
-        <MuiTextFields id="mod-login-email" label="Email" type="email" name="email"/>
-        <MuiTextFields id="mod-login-password" label="Password" type="password" name="password"/>
-        <div>
-        <Button size="large" variant="contained" color="primary" classes={ {root: classes.button}}>
-          Sign in
-        </Button>
-        </div>
-        <div>
-        <Button size="large" variant="contained" classes={ {root: classes.googleButton}}>
-          Login With Google
-        </Button>
-        </div>
-      </Paper>
-    </div>
-  );
 }
+
+function mapStateToProps({loginReducer}) {
+  return {loginReducer};
+}
+
+export default withStyles(loginStyles)(connect(mapStateToProps)(Login));
---
+++ client/src/reducers/loginReducer.js
@@ -0,0 +1,3 @@
+export default (state = {}, action) => {
+    return {email: "dasdad@dasd.com", password:"******"}
+}
