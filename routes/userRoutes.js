2e4ec6d6705c5e46e31058bda8a512d2c50b7ae8
Zakaria Hassan <zakariahassan00@hotmail.com>
2019-11-20 19:43:49 +0200

    session test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -179,21 +179,4 @@ module.exports = app => {
 
     res.send({ items: ratelist, count });
   });
-
-  app.get("/api/user/me", function(req, res) {
-    var token = req.headers["x-access-token"];
-    if (!token)
-      return res
-        .status(401)
-        .send({ auth: false, message: "No token provided." });
-
-    jwt.verify(token, config.secret, function(err, decoded) {
-      if (err)
-        return res
-          .status(500)
-          .send({ auth: false, message: "Failed to authenticate token." });
-
-      res.status(200).send(decoded);
-    });
-  });
 };
