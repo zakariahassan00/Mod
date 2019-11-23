568b1fe745fc0dc76bb6d7d9f2491cbd11abb592
Zakaria Hassan <zakariahassan00@hotmail.com>
2019-11-23 04:43:16 +0200

    deploy on Heroku

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
--- index.js
+++ index.js
@@ -44,7 +44,18 @@ require("./routes/userRoutes")(app);
 require("./routes/moviesRoutes")(app);
 require("./routes/authRouter")(app);
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// ====> DEPLOYMENT SETTINGS
+// deployment with react app settings
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets like our main.js file, or main.css file!.
+  app.use(express.static("client/build"));
+
+  // express will serve index.html if it doesn`t recogize te route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
